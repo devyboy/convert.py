87c1102cb3a7154056c1f6e8bf05b794c19237c6
Devon Pirestani <dev@udel.edu>
2019-03-24 20:51:29 -0400

    fixed an oopsie + string formatting

--- convert.py
+++ convert.py
@@ -7,64 +7,64 @@ def decToBinary(num):
     try:
         return bin(int(num)).replace("0b", "")
     except ValueError:
-        return "Invalid decimal number\n" 
+        return "\nError: Invalid decimal number\n" 
 
 def decToHex(num):
     try:
         return "0x" + hex(int(num)).split('x')[-1]
     except ValueError:
-        print("Invalid decimal number\n")
+        print("\nError: Invalid decimal number\n")
         exit()
 
 def binToDecimal(num):
     try:
         return int(num, 2)
     except ValueError:
-        print("Invalid binary number\n")
+        print("\nError: Invalid binary number\n")
         exit()
 
 def binToHex(num):
     try:
         return hex(int(num, 2))
     except ValueError:
-        print("Invalid binary number\n")
+        print("\nError: Invalid binary number\n")
         exit()
 
 def hexToDec(num):
     try:
         return int(num, 16)
     except ValueError:
-        print("Invalid hex number\n")
+        print("\nError: Invalid hex number\n")
         exit()
 
 def hexToBin(num):
     try:
         return bin(int(num, 16))[2:]
     except ValueError:
-        print("Invalid binary number\n")
+        print("\nError: Invalid binary number\n")
         exit()
     
 def main():
-    helpText = "Please enter a number and what to convert it to\n"
+    helpText = "\nError: Please enter a number and what to convert it to\n"
     if len(sys.argv) < 3:
         print(helpText)
     else:
         convert = sys.argv[1]
         number = sys.argv[2]
         if convert == "-db":
-            print(decToBinary(number) + "\n")
+            print(str(decToBinary(number)) + "\n")
         elif convert == "-dh":
-            print(decToHex(number) + "\n")
+            print(str(decToHex(number)) + "\n")
         elif convert == "-bd":
-            print(binToDecimal(number) + "\n")
+            print(str(binToDecimal(number)) + "\n")
         elif convert == "-bh":
-            print(binToHex(number) + "\n")
+            print(str(binToHex(number)) + "\n")
         elif convert == "-hd":
-            print(hexToDec(number) + "\n")
+            print(str(hexToDec(number)) + "\n")
         elif convert == "-hb":
-            print(hexToBin(number) + "\n")
+            print(str(hexToBin(number)) + "\n")
         else:
-            print("Conversion not supported\n")
+            print("\nError: Conversion not supported\n")
 
 if __name__ == "__main__":
     main()
